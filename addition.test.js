ac7b02f212bd57d9fc8b78396f73210b1dcf5ca6
Andrew MAHE <technique@andrewmahe.com>
2025-12-07 17:11:56 +0100

    test

--- addition.test.js
+++ addition.test.js
@@ -2,7 +2,7 @@ const { default: expect } = require('expect');
 const { calculerAddition, appliquerReduction } = require('./addition');
 
 test('plat à 15€ + boisson à 5€ = 20€', () => {
-  expect(calculerAddition(15, 5)).toBe(25);
+  expect(calculerAddition(15, 5)).toBe(20);
 });
 
 test('plat à 15€ + boisson à 5€ ne doit pas égaler 25€', () => {
